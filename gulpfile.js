33b384ae6d31372e33f55e15f3cdb437139bcc6a
Александр Торопов <toropovsite@yandex.ru>
2024-08-11 23:54:17 +0300

    Добавление комментариев и изменение форматирования

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
-const gulp = require('gulp');
+const gulp = require('gulp');                                   // Подключаем Gulp
 const { src, dest, series, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const rm = require('gulp-rm');                         // Очистка директорий 
-const gulpIf = require('gulp-if');
-const concat = require('gulp-concat');                 // Объединение стилей
-const browserSync = require('browser-sync').create();  // Live-сервер для разработки
-const sassGlob = require('gulp-sass-glob');            // Импорт стилей
-const reload = browserSync.reload;
-const autoprefixer = require("gulp-autoprefixer");
-const gcmq = require('gulp-group-css-media-queries');  // Группировка медия запросов
-const cleanCSS = require('gulp-clean-css');            // Минификация CSS
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');                   // Подключение Babel
-const uglify = require('gulp-uglify');                 // Минификация JavaScript 
-const imagemin = require('gulp-imagemin');             // Оптимизация изображений 
+const rm = require('gulp-rm');                                  // Очистка директорий 
+const gulpIf = require('gulp-if');                              // Для создания условий в gulp-тасках
+const concat = require('gulp-concat');                          // Объединение стилей
+const browserSync = require('browser-sync').create();           // Live-сервер для разработки
+const sassGlob = require('gulp-sass-glob');                     // Импорт стилей
+const reload = browserSync.reload;                              // Перезагрузка live - сервера при разработке
+const autoprefixer = require('gulp-autoprefixer');              // CSS autoprefixer
+const gcmq = require('gulp-group-css-media-queries');           // Группировка медия запросов
+const cleanCSS = require('gulp-clean-css');                     // Минификация CSS
+const sourcemaps = require('gulp-sourcemaps');                  // Генерация sourcemap
+const babel = require('gulp-babel');                            // Подключение Babel
+const uglify = require('gulp-uglify');                          // Минификация JavaScript 
+const imagemin = require('gulp-imagemin');                      // Оптимизация изображений 
 
+// Переменные среды "dev" или "prod"
 let env = process.env.NODE_ENV;
 
+// Подключение файла конфигов
 const {DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS} = require('./gulp.config');
 
 /**
@@ -32,22 +34,22 @@ function clearDist() {
  */
 async function compileScss() {
     src([...STYLES_LIBS, './src/sass/style.scss'])
-    .pipe(gulpIf(env === 'dev', sourcemaps.init()))
-    .pipe(sassGlob())
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulpIf(env === 'dev',
-        autoprefixer({
-            cascade: false
-        })
-    ))
-    .pipe(gulpIf(env === 'prod', gcmq()))
-    .pipe(concat('style.css'))
-    .pipe(dest('./dist/css'))
-    .pipe(gulpIf(env === 'prod', cleanCSS({compatibility: 'ie8'})))
-    .pipe(concat('style.min.css'))
-    .pipe(gulpIf(env === 'prod', sourcemaps.write('')))            // Раскомментировать, когда понядобятся sourcemap при разработке
-    .pipe(dest('./dist/css'))
-    .pipe(reload({ stream: true }));         // Перезагрузка live-сервера
+      .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+      .pipe(sassGlob())
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(gulpIf(env === 'dev',
+          autoprefixer({
+              cascade: false
+          })
+      ))
+      .pipe(gulpIf(env === 'prod', gcmq()))
+      .pipe(concat('style.css'))
+      .pipe(dest('./dist/css'))
+      .pipe(gulpIf(env === 'prod', cleanCSS({compatibility: 'ie8'})))
+      .pipe(concat('style.min.css'))
+      .pipe(gulpIf(env === 'prod', sourcemaps.write('')))            
+      .pipe(dest('./dist/css'))
+      .pipe(reload({ stream: true }));         // Перезагрузка live-сервера
 }
 
 /**
@@ -55,15 +57,15 @@ async function compileScss() {
  */
 function compScripts() {
     return src([...JS_LIBS, './src/js/*.js'])
-    .pipe(sourcemaps.init())                  // Инициализация sourcemap
-    .pipe(concat('main.js', {newLine: ";"}))  // Склеиваем JavaScript
-    .pipe(babel({
-        presets: ['@babel/env']
-    }))
-    .pipe(uglify())                           // Минификация JavaScript 
-    .pipe(sourcemaps.write(''))               // Сохранение sourcemap
-    .pipe(dest('./dist/js'))                  // Сохранение JavaScript
-    .pipe(reload({ stream: true }));          // Перезагрузка live-сервера
+      .pipe(sourcemaps.init())                  // Инициализация sourcemap
+      .pipe(concat('main.js', {newLine: ";"}))  // Склеиваем JavaScript
+      .pipe(babel({
+          presets: ['@babel/env']
+      }))
+      .pipe(uglify())                           // Минификация JavaScript 
+      .pipe(sourcemaps.write(''))               // Сохранение sourcemap
+      .pipe(dest('./dist/js'))                  // Сохранение JavaScript
+      .pipe(reload({ stream: true }));          // Перезагрузка live-сервера
 }
 
 /**
@@ -82,8 +84,8 @@ function servStart() {
  */
 function copyHtml() {
     return src('./src/*.html')
-    .pipe(dest('./dist'))
-    .pipe(reload({ stream: true }));
+      .pipe(dest('./dist'))
+      .pipe(reload({ stream: true }));
 }
 
 /**
